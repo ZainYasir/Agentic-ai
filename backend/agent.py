6439ba3cf4e9bc7dc77806f7cd457dd24a691612
ZainYasir <itszainyasir@gmail.com>
2025-09-23 16:16:25 +0500

    updated agent.py

--- backend/agent.py
+++ backend/agent.py
@@ -1,15 +1,8 @@
-from langchain_community.llms import HuggingFacePipeline
-from transformers import pipeline
+# backend/agent.py
+from langchain_huggingface import HuggingFacePipeline
 from models.phi_model import load_phi2
 
 def get_langchain_llm():
-    model, tokenizer = load_phi2()
-    pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        max_new_tokens=200,
-        temperature=0.7,
-        do_sample=True
-    )
-    return HuggingFacePipeline(pipeline=pipe)
+    pipe = load_phi2()
+    llm = HuggingFacePipeline(pipeline=pipe)
+    return llm
