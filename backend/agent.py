d39e5652c91a32057949ad95666b97695be22a06
ZainYasir <itszainyasir@gmail.com>
2025-09-23 16:25:13 +0500

    updated agent.py 2

--- backend/agent.py
+++ backend/agent.py
@@ -1,5 +1,4 @@
-# backend/agent.py
-from langchain_huggingface import HuggingFacePipeline
+from langchain_community.llms import HuggingFacePipeline
 from models.phi_model import load_phi2
 
 def get_langchain_llm():
