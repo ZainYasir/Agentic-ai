b1e96cab121aa851b3410c7964daf2c32baae6dd
ZainYasir <itszainyasir@gmail.com>
2025-09-24 14:54:34 +0500

    updated main and req

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
+# backend/main.py
+from fastapi import FastAPI
+from pydantic import BaseModel
 from agent import get_langchain_llm
 
-if __name__ == "__main__":
-    llm = get_langchain_llm()
-    question = "Explain reinforcement learning in simple words."
-    response = llm.invoke(question)
+# init FastAPI
+app = FastAPI()
 
-    print("Q:", question)
-    print("A:", response)
+# load model once
+llm = get_langchain_llm()
+
+# request schema
+class ChatRequest(BaseModel):
+    message: str
+
+@app.get("/")
+def root():
+    return {"message": "Agentic AI Backend is running"}
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    response = llm.invoke(request.message)
+    return {"question": request.message, "answer": response}
